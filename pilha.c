28dcf0e6192c71279a631f31a1ecdc3d77bfca8c
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-10-06 21:50:25 -0300

    changes

--- pilha.c
+++ pilha.c
@@ -4,19 +4,30 @@
 #include "pilha.h"
 
 
-void empilhar(p_pilha p, int i) {
-	p->v[p->topo] = i;
-	p->topo++;
+p_pilha empilhar(p_pilha p, int i) {
+	p_no new = malloc(sizeof(No));
+	new->number = i;
+	new->next = p->top;
+	p->top = new;
+	return p;
 }
 
 int desempilhar(p_pilha p) {
-	p->topo--;
-	return p->v[p->topo];
+	if (p->top->number == -1) {
+		return -1;	
+	}
+	p_no top = p->top;
+	int x = top->number;
+	p->top = p->top->next;
+	free(top);
+	return x;
 }
 
-p_pilha create_pilha(int size) {
-	p_pilha pilha = malloc(sizeof(p_pilha));
-	pilha->v = malloc(size * sizeof(int));
-	pilha->topo = 0;
+p_pilha create_pilha() {
+	p_pilha pilha = malloc(sizeof(Pilha));
+	pilha->top = malloc(sizeof(No));
+	p_no head = pilha->top;
+	head->next = head;
+	head->number = -1;
 	return pilha;
 }
