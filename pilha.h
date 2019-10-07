42a5df15bc0deb11f9ee1247887885e6fa52284f
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-10-06 21:52:17 -0300

    new changes

--- pilha.h
+++ pilha.h
@@ -1,19 +1,23 @@
 #ifndef PILHA_H
 #define PILHA_H
+typedef struct No {
+	struct No *next;
+	int number;
+} No;
 
+typedef struct No * p_no;
 
 typedef struct {
-	int *v;
-	int topo;
+	p_no top;
 } Pilha;
 
 typedef Pilha * p_pilha;
 
-void empilhar(p_pilha p, int i);
+p_pilha empilhar(p_pilha p, int i);
 
 int desempilhar(p_pilha p);
 
-p_pilha create_pilha(int size);
+p_pilha create_pilha();
 
 
 #endif
