d5c4f6b2ab61572d4896012fcdf7bc30982c4116
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-10-06 21:50:03 -0300

    Changes

--- lab06.c
+++ lab06.c
@@ -5,35 +5,58 @@
 
 #include "pilha.h"
 
-int removeDigits(char stringNumber[], int size) {
-	for (int i = 0; i < size; i++) {
-		printf("%c", stringNumber[i]);	
-	}
-	return 0;
-}
-
 int main()
 {
-	int m, n;
+	int m;;
+	scanf("%d ", &m);
+	printf("oi 1");
+	int digitosRemovidos = 0;
 
-	scanf("%d", &m);
-	scanf("%d", &n);
-	
-	
-
-	for (int i = 1; i < 2; i++) {
-		int number;
-		scanf("%d", &number);
-		int numberOfDigits = (int)((ceil(log10(number)))*sizeof(char));
-		p_pilha pilha = create_pilha(numberOfDigits);
-		for (int i = 0; i < numberOfDigits; number = number - (number % 10) / 10) {
-			empilhar(pilha, number % 10);		
+	for (int i = 1; i <= m; i++) {
+		long number;
+		int n;
+		scanf("%ld ", &number);
+		printf("oi 2");
+		scanf("%d", &n);
+		printf("oi 3");
+		int numberOfDigits = floor(log10(abs(number)));
+		printf("%d", numberOfDigits);
+		
+		p_pilha pilha = create_pilha();
+		for (int i = 0; i < numberOfDigits; number = (number - (number % 10)) / 10, i = i + 1) {
+			pilha = empilhar(pilha, number % 10);	
 		}
-		for (int i = 0; i < numberOfDigits; i++) {
-			printf("%d", pilha->v[i]);
+		int primeiroValor = desempilhar(pilha);
+		printf("%d", primeiroValor);
+		p_pilha segundaPilha = create_pilha();
+		if(primeiroValor >= 0) {
+			empilhar(segundaPilha, primeiroValor);
+			for (int i = 1; i <= numberOfDigits - 1; i = i + 1) {
+				int proximoValor = desempilhar(pilha);
+				int valorSalvo = desempilhar(segundaPilha);
+				if (proximoValor > valorSalvo) {
+					digitosRemovidos += 1;
+					empilhar(segundaPilha, proximoValor);
+				} else {
+					empilhar(segundaPilha, valorSalvo);
+					empilhar(segundaPilha, proximoValor);
+				}
+			}
+			if (digitosRemovidos != n) {
+				for (; digitosRemovidos != n;) {
+					desempilhar(segundaPilha);
+					digitosRemovidos = digitosRemovidos + 1;
+				}
+			}
+		}
+		p_pilha terceiraPilha = create_pilha();
+		for (int i = 1; i <= numberOfDigits - n; i++) {
+			empilhar(terceiraPilha, desempilhar(segundaPilha));
+		}
+		for (int i = 1; i <= numberOfDigits - n; i++) {
+			printf("%d", desempilhar(terceiraPilha));
 		}
 	}
 	
 	return EXIT_SUCCESS;
 }
-
