a94ff2e5de9dee8a4fc55e70819d4f0e6c3db7a1
danilopereira10 <54330234+danilopereira10@users.noreply.github.com>
2019-10-08 21:50:27 -0300

    Backup lab06

--- lab06.c
+++ lab06.c
@@ -7,42 +7,50 @@
 
 int main()
 {
-	int m;;
-	scanf("%d ", &m);
-	printf("oi 1");
-	int digitosRemovidos = 0;
+	int m;
+	scanf("%d", &m);
 
 	for (int i = 1; i <= m; i++) {
-		long number;
+		int digitosRemovidos = 0;
+		int numeroDeDigitos;
+		//unsigned long long number;
 		int n;
-		scanf("%ld ", &number);
-		printf("oi 2");
-		scanf("%d", &n);
-		printf("oi 3");
-		int numberOfDigits = floor(log10(abs(number)));
-		printf("%d", numberOfDigits);
-		
+		//scanf("%llu", &number);
 		p_pilha pilha = create_pilha();
-		for (int i = 0; i < numberOfDigits; number = (number - (number % 10)) / 10, i = i + 1) {
-			pilha = empilhar(pilha, number % 10);	
+		//int numberOfDigits = floor(log10(abs(number))) + 1;
+		char caracter='-';
+		while (caracter != ' ') {
+			scanf("%c", &caracter);
+			//pilha = empilhar(pilha, number % 10);
 		}
+		scanf("%d ", &n);
+
+		for (int i = 0; i < numberOfDigits; number = (number - (number % 10)) / 10, i = i + 1) {
+			printf("%d", desempilhar(pilha));	
+		}
+		/*for (int i = 0; number > 0; number = (number - (number % 10)) / 10, i = i + 1) {
+				
+		} */
+		printf("\n%d Number of digits", numberOfDigits);
+		//Funcionando até aqui!!
 		int primeiroValor = desempilhar(pilha);
-		printf("%d", primeiroValor);
 		p_pilha segundaPilha = create_pilha();
+		int proximoValor;
+		//printf("lugar 2");
 		if(primeiroValor >= 0) {
 			empilhar(segundaPilha, primeiroValor);
-			for (int i = 2; i <= numberOfDigits; ) {
-				int proximoValor = desempilhar(pilha);
+			for (int i = 2; i <= numberOfDigits && digitosRemovidos != n; ) {
+				proximoValor = desempilhar(pilha);
 				int continuaDesempilhando = 1;
-				while (continuaDesempilhando) {
+				while (continuaDesempilhando && digitosRemovidos != n) {
 					int valorSalvo = desempilhar(segundaPilha);
 					if (valorSalvo == - 1) {//pilha vazia
+						empilhar(segundaPilha, proximoValor);
 						break;
 					}
 					if (proximoValor > valorSalvo) {
-						digitosRemovidos += 1;
-						empilhar(segundaPilha, proximoValor);
 						i = i + 1;
+						digitosRemovidos += 1;
 					} else {
 						continuaDesempilhando = 0;
 						empilhar(segundaPilha, valorSalvo);
@@ -51,12 +59,28 @@ int main()
 					}
 				}
 			}
-			if (digitosRemovidos != n) {
-				for (; digitosRemovidos != n;) {
-					desempilhar(segundaPilha);
-					digitosRemovidos = digitosRemovidos + 1;
+			//pro 1 deveria funcionar tranquilo
+			//pro 10 também
+			//pro 100 também
+			//pro 999 também
+			//pro 7589
+			if (digitosRemovidos == n && n > 0) {
+				empilhar(segundaPilha, proximoValor);
+			}
+			while (digitosRemovidos != n) {
+				desempilhar(segundaPilha);
+				digitosRemovidos = digitosRemovidos + 1;
+				//printf("lugar 4");
+			}
+			if (digitosRemovidos == n) {
+				int proximoValor = desempilhar(pilha);
+				while (proximoValor != - 1) {
+					empilhar(segundaPilha, proximoValor);
+					proximoValor = desempilhar(pilha);
+					//printf("lugar 2");
 				}
 			}
+			
 		}
 		p_pilha terceiraPilha = create_pilha();
 		for (int i = 1; i <= numberOfDigits - n; i++) {
@@ -65,6 +89,7 @@ int main()
 		for (int i = 1; i <= numberOfDigits - n; i++) {
 			printf("%d", desempilhar(terceiraPilha));
 		}
+		printf("\n");	
 	}
 	
 	return EXIT_SUCCESS;
